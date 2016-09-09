d2a93be4f721c11d32c231c2f1c00145acb239b0
Colin Swaney <colinswaney@gmail.com>
2016-09-08 22:20:10 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 
-with open('README') as f:
+with open('README.md') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
@@ -11,12 +11,14 @@ with open('LICENSE') as f:
 
 setup(
     name='hfttools',
-    version='0.0.1',
+    version='0.0.2',
     description='A package for high-frequency trade research.',
     long_description=readme,
     author='Colin Swaney',
     author_email='colin-swaney@uiowa.edu',
     url='https://github.com/colinswaney/hfttools',
     license=license,
+    classifiers=['Development Status :: 3 - Alpha','Programming Language :: Python :: 3.5'],
+    install_requires=['numpy', 'h5py', 'psycopg2', 'pandas'],
     packages=find_packages(exclude=('tests', 'docs'))
 )
